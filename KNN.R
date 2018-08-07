867274a564fe6d879ea3adc1d416852550ac79fa
James Bowers <30636919+jbowers85@users.noreply.github.com>
2018-08-07 14:19:37 -0700

    Update KNN.R

--- KNN.R
+++ KNN.R
@@ -15,7 +15,7 @@ library(caret)
 library(kknn)
 
 
-## 1.) K NEAREST NEIGHBORS - ORDINAL DEPENDENT VARIABLE ####
+## 1.) K NEAREST NEIGHBORS - CLASSIFICATION - ORDINAL DEPENDENT VARIABLE ####
 
 ### Get Data ####
 
@@ -28,7 +28,6 @@ redData <- read.csv(redUrl, header=TRUE , sep=';')
 head(redData)
 whiteData <- read.csv(whiteUrl, header=TRUE , sep=';')
 head(whiteData)
-
 
 ### Cleanse ####
 
@@ -73,8 +72,8 @@ colnames(corrMatrix)[highlyCorr] # print the columns to remove according to cuto
 ### Split Data ####
 
 #### split data by color
-whiteData <- allData[which(allData$color == 'white'),]
-redData <- allData[which(allData$color == 'red'),]
+whiteData <- allData[which(allData$color == 'white'), -1]
+redData <- allData[which(allData$color == 'red'), -1]
 
 #### create train and test set 
 ##### Red
@@ -95,11 +94,11 @@ nrow(whiteTestData)/nrow(whiteTrainData)
 
 #### verify split proportions
 par(mfrow=c(2,2)) 
-barplot(table(redTrainData$quality) / nrow(redTrainData), col='indianred3', main="Red: Train") # proportion of quality
-barplot(table(redTestData$quality) / nrow(redTestData), col='indianred3', main="Red: Test") # proportion of quality
+barplot(table(redTrainData$quality) / nrow(redTrainData), col='indianred3', main="Red: Train")
+barplot(table(redTestData$quality) / nrow(redTestData), col='indianred3', main="Red: Test")
 
-barplot(table(whiteTrainData$quality) / nrow(whiteTrainData), col='peachpuff', main="White: Train") # proportion of quality
-barplot(table(whiteTestData$quality) / nrow(whiteTestData), col='peachpuff', main="White: Test") # proportion of quality
+barplot(table(whiteTrainData$quality) / nrow(whiteTrainData), col='peachpuff', main="White: Train") 
+barplot(table(whiteTestData$quality) / nrow(whiteTestData), col='peachpuff', main="White: Test") 
 par(mfrow=c(1,1)) 
 
 #### reset factors for red since red has no '9' quality scores
@@ -119,7 +118,7 @@ redTestData.processed <- predict(preProcess(redTestData, method=c("center", "sca
 summary(redTestData.processed)
 
 kknn_fit_red <- train.kknn(quality ~ fixed.acidity + volatile.acidity + citric.acid + residual.sugar + chlorides + 
-                   free.sulfur.dioxide + total.sulfur.dioxide + density  + pH + sulphates + alcohol + color,
+                   free.sulfur.dioxide + total.sulfur.dioxide + density  + pH + sulphates + alcohol,
                   data = redTrainData.processed, 
                   kmax=25, distance=1, kernel=c("triangular", "gaussian", "rank", "rectangular", "epanechnikov", "optimal"), 
                   contrasts = c(unordered = "contr.dummy", ordered = "contr.ordinal"))
@@ -138,7 +137,7 @@ whiteTestData.processed <- predict(preProcess(whiteTestData, method=c("center", 
 summary(whiteTestData.processed)
 
 kknn_fit_white <- train.kknn(quality ~ fixed.acidity + volatile.acidity + citric.acid + residual.sugar + chlorides + 
-                         free.sulfur.dioxide + total.sulfur.dioxide + density  + pH + sulphates + alcohol + color,
+                         free.sulfur.dioxide + total.sulfur.dioxide + density  + pH + sulphates + alcohol,
                        data = whiteTrainData.processed, 
                        kmax=25, distance=1, kernel=c("triangular", "gaussian", "rank", "rectangular", "epanechnikov", "optimal"), 
                        contrasts = c(unordered = "contr.dummy", ordered = "contr.ordinal"))
@@ -176,42 +175,47 @@ plot(kknn_fit_white)
 ### Evaluate ####
 
 #### Red
-cm.kknn.red <- table(predict(kknn_fit_red, redTestData.processed[,-13]), redTestData$quality)
+cm.kknn.red <- table(predict(kknn_fit_red, redTestData.processed[,-12]), redTestData$quality)
 cm.kknn.red
 
 acc.kknn.red <- sum(diag(cm.kknn.red))/sum(cm.kknn.red)
 acc.kknn.red
 
-redTestData$prediction <- predict(kknn_fit_red, redTestData.processed[,-13])
+redTestData$prediction <- predict(kknn_fit_red, redTestData.processed[,-12])
 redTestData$pred_diff <- as.numeric(redTestData$quality) - as.numeric(redTestData$prediction)
-barplot(table(redTestData$pred_diff))
+barplot(table(redTestData$pred_diff), col='indianred3', main="Red: Prediction Error")
 
 #### White
-cm.kknn.white <- table(predict(kknn_fit_white, whiteTestData.processed[,-13]), whiteTestData$quality)
+cm.kknn.white <- table(predict(kknn_fit_white, whiteTestData.processed[,-12]), whiteTestData$quality)
 cm.kknn.white
 
 acc.kknn.white <- sum(diag(cm.kknn.white))/sum(cm.kknn.white)
 acc.kknn.white
 
-whiteTestData$prediction <- predict(kknn_fit_white, whiteTestData.processed[,-13])
+whiteTestData$prediction <- predict(kknn_fit_white, whiteTestData.processed[,-12])
 whiteTestData$pred_diff <- as.numeric(whiteTestData$quality) - as.numeric(whiteTestData$prediction)
-barplot(table(whiteTestData$pred_diff))
+barplot(table(whiteTestData$pred_diff), col='peachpuff', main="White: Prediction Error")
 
 ### Predict ####
 
 #### define new record
-newData <- data.frame(color=c("red","white"), fixed.acidity=0, volatile.acidity=0, citric.acid=0, residual.sugar=0, chlorides=0, 
-                  free.sulfur.dioxide=0, total.sulfur.dioxide=0, density=0, pH=0, sulphates=0, alcohol=0)
-newData
-
+newRedData <- data.frame(fixed.acidity=15, volatile.acidity=0.7, citric.acid=0.2, residual.sugar=2.8, chlorides=0.09, 
+                  free.sulfur.dioxide=20, total.sulfur.dioxide=63, density=0.996, pH=6, sulphates=0.8, alcohol=8.9)
+newRedData
 
 #### standardize using column means and st dev from original non-normalized dataset 
+standardizeNewData <- function(new,old) {
+  oldColumnMeans <- apply(old,2,mean)
+  oldColumnStDev <- apply(old,2,sd)
+  
+  newStandardized <- (new - oldColumnMeans) / oldColumnStDev
+  
+  return(newStandardized)
+}
+  
+  
+newRedData.processed <- standardizeNewData(newRedData, redTestData[c(-12,-13,-14)])
 
-str(newData)
 
-predict(kknn_fit_red,newData)
-predict(kknn_fit_white,newData)
+predict(kknn_fit_red,newRedData.processed)
 
-head(testData.processed[,-13])
-
-options('contrasts')
